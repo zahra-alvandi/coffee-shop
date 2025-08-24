ae61bad7e23d674ea6a86a705dcfd83ba23f65e0
Zahra Alvandi <zalvandi39@gmail.com>
2025-08-24 22:21:50 +0330

    theme and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./public/scripts/**/*.{js,ts}"
-  ],
   theme: {
     extend: {
       colors: {
         brand: "#1E40AF",
         accent: "#F59E0B",
       },
+      boxShadow: {
+        normal: "0px 1px 10px rgba(0,0,0,.05)",
+      },
+      borderRaduis: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMeduim: "Dana Meduim",
+        DanaDemiBold: "Dana DemiBold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMeduim: "Morabba Meduim",
+        MorabbaBold: "Morabba Bold",
+      },
+      letterSpacing: {
+        tightest: "-0.065em",
+      },
+      spacing: {
+        4.5: "1.125rem",
+        25: "6.25rem",
+        30: "7.5rem",
+        50: "12.5rem",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "0.625rem"
+        }
+      },
+      backgroundImage: {
+        "home-mobile": "url('../images/headerBgMobile.webp')",
+        "home-desktop": "url('../images/headerBgDesktop.webp')",
+      },
+    },
+
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    }
+  ],
 }
