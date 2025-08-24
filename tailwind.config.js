d2b32aa1f9130b41be6c3189ab6e25df8fc396ce
Zahra Alvandi <zalvandi39@gmail.com>
2025-08-24 19:03:04 +0330

    coffee shop install and config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/scripts/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1E40AF",
+        accent: "#F59E0B",
+      },
+    },
+  },
+  plugins: [],
+}
